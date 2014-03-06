51b0496ca5118b08970d55c0af45745a1fb6515e
plambson <plambson@dmail.dixie.edu>
2014-03-06 14:16:19 -0700

    Commiting basic footer css.

--- class/about.php
+++ class/about.php
@@ -1,4 +1,31 @@
-<div id="wrapper">
 <?php include_once('head.php'); ?>
-tit
+<div class="content">
+    <div class="listheads" id="title">
+        Thank you for using DSU Classifieds!
+    </div>
+    <div class="bodytext">
+    This website was developed by a small team of DSUSA Students at the request of Gregory 
+    Layton, the Student Body President for the 2014-2015 academic year. This site is to aid students that are 
+    hurting for money by allowing them to sell things they do need and to buy things at lower costs. The site 
+    was mainly made for textbooks, but we encourage you to sell anything else as well. <br><br>
+    <span class="tab"></span>The site is reserved for Dixie State University Students, so only email addresses containing 
+    "dmail.dixie.edu" will be allowed to register. All posts are private to the outside world, only Dixie 
+    
+    Students and the admins may view them. Please note that this site does not condone the sale of any 
+    
+    illegal items or firearms. We will remove any posts containing items that may get somebody in trouble. <br><br>
+    
+    <span class="tab"></span>We take your privacy seriously, so every post will only have a lifetime of four months before it is 
+    
+    deleted forever. We will also not store any of your personal information except for your email address. 
+    
+    You may however include your personal info in the post, but we will not make any guarantees of privacy 
+    
+    if it is posted. If you get harassed, delete your post and contact the Dean of Students.<br><br> 
+    
+    <span class="tab"></span>This site was developed by Alex, Anthie, Colten, and Tyler. Alex will continue to develop it after 
+    
+    the initial launch and any feature requests should be sent to plambson@dmail.dixie.edu
+    </div><!--close bodytext-->
+</div><!--close content-->
 <?php include_once('foot.php'); ?>
--- class/foot.php
+++ class/foot.php
@@ -1,7 +1,14 @@
 <link rel="stylesheet" href="foot.css">
-<div id="footerwrapper">
-
-</div>
-</div>
+    <div id="footerwrapper">
+    	<div class="columnb">
+        	<ul>
+            	<li><a href="index.php">Home</a></li>
+                <li><a href="listings.php">Listings</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </div>
+    
+    </div><!--close foot wrapper-->
+</div><!--closes wrapper-->
 </body>
 </html>
