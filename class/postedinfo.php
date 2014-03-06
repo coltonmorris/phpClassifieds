e74e4e7e4b1e265a325f2253e4f62e73b941463d
Colton <coltonmorris94@gmail.com>
2014-03-06 15:37:58 -0700

    Who even uses this? btw, alex is gay

--- class/postedinfo.php
+++ class/postedinfo.php
@@ -17,23 +17,3 @@ show_table($results);
  </p>
 </div> <!-- end middlecolumn -->
 <?php include_once('foot.php') ?>
-=======
-<?php include_once('head.php') ?>
-<div id="middlecolumn">
-<p>
-	Thank you for posting your ad! Hopefully some shmuck will fall for your scam. TROLOLol0l.
-
-<?php
-//content with quotes buggs table
-$username = $_SESSION['username'];
-$content = "SUBJECT: $_POST[Subject] ";
-$content .= "DESCRIPTION: $_POST[Description] ";
-$content .= "COST: $_POST[Cost] ";
-$content .= "CATEGORY: $_POST[Category] ";
-$query = "insert into jobs (username,description) values ('$username','$content')";
-$results = do_query($query);
-show_table($results);
-?>
- </p>
-</div> <!-- end middlecolumn -->
-<?php include_once('foot.php') ?>
