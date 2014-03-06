a341684435969469523781815f063ac68e945a58
Alex Lambson <plambson@dmail.dixie.edu>
2014-03-05 17:10:53 -0700

    Update head.php
    Made it beautiful

--- head.php
+++ head.php
@@ -4,20 +4,14 @@
 <link rel="stylesheet" href="main.css">
 <body>
 <?php session_start(); ?>
-<h1><a href='index.php' id="title">Dixie State Job Board</a></h1>
-<div id="nav">
-	<ul>
-		<li><a href="listings.php">Listings</a></li>
-		<li><a href="login.php">Login</a></li>
-		<li><a href="about.php">About</a></li>
-		<li><a href="post.php">Post an Ad</a></li>
-		<?php
-			if (isset($_SESSION['control_panel'])){
-				echo "<li><a href=$_SESSION[control_panel]>Control Panel</a></li>";
-			}
-		?>
-	</ul>
-</div> <!-- nav end -->
+<div id="navwrapper">
+            <a href="index.html"><img src="{% static "DSULogo.png" %}" id="homebutton" alt="DIXIE Classifieds"></a>
+            <nav id="nav">
+                <a href="{% url "home" %}" alt="" class="buttons">Home</a>
+                <a href="{% url "listings" %}" alt="" class="buttons" id="level2button">Listings</a>
+                <a href="{% url "about" %}" alt="" class="buttons" id="level3button">About</a>
+            </nav>
+        </div>
 <?php
 include_once('session.php');
 include_once('db_functions.php');
