5ee1b1bb316e443a30794b9aad11ea043061d367
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 15:57:59 -0600

    force login

--- class/post.php
+++ class/post.php
@@ -5,16 +5,27 @@
 		Subject: <input type="text" name="subject"><br/>
 		Description: <textarea name="description" cols="65" rows="10"></textarea>
 		Cost: <input type="text" name="cost"><br/>
-		Category:<select name ="Category">
+		Catagory:<select name ="catagory">
 				<!-- this list will be pulled from the database -->
 			 <?php 
 				$query = "select name from catagories";
 				$results = do_query($query);
 				while ($row = mysqli_fetch_row($results)){
-					echo "$row[0]".": <option value='"."$row[0]"."'>"."$row[0]</option>";
+					echo "$row[0]:<option value='$row[0]'>$row[0]</option>";
 					} 
 				?>
 			</select><br/>
+			Subcatagory:<select name ="subcatagory">
+			 <?php 
+			 	//parent should be the currently selected option in the catagory select
+				$query = "select name from catagories where parent='Textbooks'";
+				$results = do_query($query);
+				while ($row = mysqli_fetch_row($results)){
+					echo "$row[0]:<option value='$row[0]'>$row[0]</option>";
+					} 
+				?>
+			</select><br/>
+
 		<input type="submit" name="submit" value="Post Ad">
 	</p>
 </form>
