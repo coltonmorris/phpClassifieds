fd908130a06bcc1694a198c0e1d003871beac15f
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 15:14:33 -0600

    force login

--- class/index.php
+++ class/index.php
@@ -18,41 +18,59 @@
           });
 
         });
-      </script>
+</script>
 <div class="content">
+<!-- open column div. open listhead div. catagory is in listheads.
+		close listhead. open subcats ul. each subcatagory is in <li></li>
+		close ul. close column.
+-->
+<?php 
+$query = "select name from catagories";
+$results = do_query($query);
+show_catagories($results);
+function show_catagories($results) {
+	while ($row = mysqli_fetch_assoc($results)){
+		foreach ($row as $k => $val){
+			echo $k . ' ' . $val;
+		}
+	}
+}
+//		echo "<div class='column'>";
+//			echo "<div class='listheads'>";
+//				echo $
+?>
 	<div class="column">
-            <div class="listheads">
-                Electronics
-                </div>
-                <ul class="subcats">
-                	<li>Phones</li>
-                    <li>Tablets</li>
-                    <li>Computers and stuff</li>
-                </ul>
-                    <!--close a listhead-->
-            </div><!--close the column-->
-            <div class="column">
-            	<div class="listheads">
-                Appliances
-                </div>
-                <ul class="subcats">
-                	<li>Whacky waving inflatable arm flailing tube men</li>
-                    <li>flashlights</li>
-                    <li>blenders</li>
-                </ul>
-            </div>
-            <div class="column">
-            	<div class="listheads">
-                	Vehicles
-                </div>
-                <ul class="subcats">
-                	<li>Infiniti Master race</li>
-                    <li>Inferior suburu</li>
-                    <li>American <del>toys</del> cars</li>
-                </ul>
-            </div>
-            <br id="clear">
-    </div>
+				<div class="listheads">
+						Electronics
+				</div><!--close a listhead-->
+						<ul class="subcats">
+							<li>Phones</li>
+							<li>Tablets</li>
+							<li>Computers and stuff</li>
+						</ul>
+	</div><!--close the column-->
+				<div class="column">
+					<div class="listheads">
+						Appliances
+						</div>
+						<ul class="subcats">
+							<li>Whacky waving inflatable arm flailing tube men</li>
+								<li>flashlights</li>
+								<li>blenders</li>
+						</ul>
+				</div>
+				<div class="column">
+					<div class="listheads">
+							Vehicles
+						</div>
+						<ul class="subcats">
+							<li>Infiniti Master race</li>
+								<li>Inferior suburu</li>
+								<li>American <del>toys</del> cars</li>
+						</ul>
+				</div>
+				<br id="clear">
+</div>
 
 <?php include_once('foot.php'); ?>
 
