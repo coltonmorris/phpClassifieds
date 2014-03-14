37381f53fddfb2b4717bd4da85c15df8ab2b04f7
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 15:33:07 -0600

    force login

--- class/index.php
+++ class/index.php
@@ -34,13 +34,13 @@ function show_catagories($results) {
 		echo "<div class='column'>";
 			echo "<div class='listheads'>";
 				echo $row[0]; //catagory name
-			echo "</div>" //close listhead
+			echo "</div>"; //close listhead
 			echo "<ul class='subcats'>";
 			//loop through each subcatagory now
 			$query = "select name from subcatagories where parent = '$row[0]'";
 			$subresults = do_query($query);
 			while ($sub = mysqli_fetch_row($subresults)){
-				echo "<li>"
+				echo "<li>";
 				echo $sub[0];
 				echo "</li>";
 			}
