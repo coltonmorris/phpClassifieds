410b3297ba67deadb0d55f9f6ce109e29db46752
Alex Lambson <underoathalex15@gmail.com>
2014-03-14 00:48:15 -0600

    removed <p>

--- class/admin_control.php
+++ class/admin_control.php
@@ -1,6 +1,5 @@
 <?php include_once('head.php'); ?>
 <div id="middlecolumn">
-<p>
 <?php
 if (!isset($_SESSION['control_panel']) || $_SESSION['control_panel'] != 'admin_control.php'){
 	header('Location: index.php');
@@ -17,6 +16,5 @@ $query = "select * from users";
 $results = do_query($query);
 show_user_admin($results);
 ?>
-</p>
 </div> <!-- end middlecolumn -->
 <?php include_once('foot.php') ?>
