68262c9afc8e3ea6168f55d4dc237e3ecc0b6644
Alex Lambson <underoathalex15@gmail.com>
2014-03-14 00:42:56 -0600

    admin page

--- class/admin_control.php
+++ class/admin_control.php
@@ -5,14 +5,14 @@
 if (!isset($_SESSION['control_panel']) || $_SESSION['control_panel'] != 'admin_control.php'){
 	header('Location: index.php');
 }
-echo "<br> Listings";
+echo "Listings<hr>";
 $username = $_SESSION['username'];
 $password = $_SESSION['password'];
 $query = "select * from jobs";
 $results = do_query($query);
 show_job_admin($results);
 echo "<hr>";
-echo "USERS";
+echo "Users<hr>";
 $query = "select * from users";
 $results = do_query($query);
 show_user_admin($results);
