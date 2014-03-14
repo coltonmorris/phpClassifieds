0a79d3705123cd0e8b3da7f82f80db6db2516a6f
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 15:31:57 -0600

    force login

--- class/index.php
+++ class/index.php
@@ -31,20 +31,25 @@ show_catagories($results);
 function show_catagories($results) {
 	//loop through each catagory
 	while ($row = mysqli_fetch_row($results)){
-		//echo "<div class='column'>";
-			//echo "<div class='listheads'>";
+		echo "<div class='column'>";
+			echo "<div class='listheads'>";
 				echo $row[0]; //catagory name
-			//echo "</div>" //close listhead
-			//echo "<ul class='subcats'>";
+			echo "</div>" //close listhead
+			echo "<ul class='subcats'>";
 			//loop through each subcatagory now
 			$query = "select name from subcatagories where parent = '$row[0]'";
 			$subresults = do_query($query);
 			while ($sub = mysqli_fetch_row($subresults)){
+				echo "<li>"
 				echo $sub[0];
+				echo "</li>";
 			}
+			echo "</ul>";
+		echo "</div>";
 	}
 }
 ?>
+<!--
 	<div class="column">
 				<div class="listheads">
 						Electronics
@@ -76,6 +81,7 @@ function show_catagories($results) {
 						</ul>
 				</div>
 				<br id="clear">
+				-->
 </div>
 
 <?php include_once('foot.php'); ?>
