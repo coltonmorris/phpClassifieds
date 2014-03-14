12b715b672df317556f1796625031a3787148706
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 00:33:01 -0600

    force login

--- class/head.php
+++ class/head.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
 if (!isset($_SESSION['allow'])){
-	if ($_SERVER['PHP_SELF'] != '/phpClassifieds/class/login.php' ||
-			$_SERVER['PHP_SELF'] != '/phpClassifieds/class/log.php' ||
+	if ($_SERVER['PHP_SELF'] != '/phpClassifieds/class/login.php' &&
+			$_SERVER['PHP_SELF'] != '/phpClassifieds/class/log.php' &&
 			$_SERVER['PHP_SELF'] != '/phpClassifieds/class/create_user.php'){
 		//header('Location: http://www.ataxicdesign.com/phpClassifieds/class/login.php');
 		echo "redirect";
