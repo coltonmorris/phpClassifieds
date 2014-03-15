28106acbbda2e4bbdb67b7a32c5e9da8d372d25a
Alex Lambson <underoathalex15@gmail.com>
2014-03-15 01:26:42 -0600

    maybe?

--- class/post.php
+++ class/post.php
@@ -8,8 +8,13 @@
         <textarea name="description" required cols="60" rows="5"></textarea><br><br>
 		<label for="cost">*Cost: </label>
         <input type="text" required name="cost"><br><br>
+        <script>
+			function giveparent(id) {
+				var parent = document.getElementById(id);	
+			}
+		</script>
 		<label for="catagory">*Category:</label>
-        <select name ="catagory" required>
+        <select name ="catagory" required onChange='giveparent("catagory")'>
 				<!-- this list will be pulled from the database -->
 			 <?php 
 				$query = "select name from catagories";
@@ -23,7 +28,8 @@
         <select name ="subcatagory" required>
 			 <?php 
 			 	//parent='blank' should be the currently selected option in the catagory select field
-				$query = "select name from subcatagories where parent='Textbooks'";
+				$parent = getElementByID('catagory');
+				$query = "select name from subcatagories where parent='$parent'";
 				$results = do_query($query);
 				while ($row = mysqli_fetch_row($results)){
 					echo "$row[0]:<option value='$row[0]'>$row[0]</option>";
