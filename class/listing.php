d72beb6da8b36e0c617385dd6c293e5997ec03d6
Alex Lambson <underoathalex15@gmail.com>
2014-03-15 15:38:24 -0600

    I made sudo code for you
    These pages will be linked from the listings page, like in craigslist.
    We need each posting to be given a unique id so we can track which ones
    are being used.

---
+++ class/listing.php
@@ -0,0 +1,25 @@
+<?php include_once('head.php');?>
+<?php
+	//sudo code
+	//<div middle column>
+	//	<div slider>
+	//		<ul of stuff>
+	//			<php code to make <li> that contain the pictures>
+	//		</ul
+	//	</div slider>
+	//	<div subject>
+	//		<php to get subject>
+	//	</div>
+	//	<div cost>
+	//		php
+	//	/div
+	//	<div body>
+	//		php
+	//	</div>
+	//	<div dmail>
+	//		php to get dmail
+	//	</div
+	//</div
+?>
+
+<?php include_once('foot.php');?>
