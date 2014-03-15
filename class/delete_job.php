b927bc50a6f198f428679e47fe34772936e2fcdb
coltonmorris <coltonmorris94@gmail.com>
2014-03-14 23:03:53 -0600

    force login

--- class/delete_job.php
+++ class/delete_job.php
@@ -3,7 +3,7 @@ include_once('db_functions.php');
 
 if(isset($_GET['id'])){
 	$id = $_GET['id'];
-	delete_row('jobs','id',$id);
+	delete_row('listings','id',$id);
 	header("Location: admin_control.php");
 }
 ?>
