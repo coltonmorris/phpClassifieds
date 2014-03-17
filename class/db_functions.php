5d40bfb767870259a18884009325022ea9234f9f
coltonmorris <coltonmorris94@gmail.com>
2014-03-16 23:27:19 -0600

    file upload

--- class/db_functions.php
+++ class/db_functions.php
@@ -17,7 +17,7 @@ function show_catagories($results) {
 		echo "<div class='column'>";
 			echo "<div class='listheads'>";
 			$link_name = $row[0] . '.php';
-				echo "<href='$link_name'>"; //catagory name
+				echo "<a href='$link_name'>"; //catagory name
 			echo "</div>"; //close listhead
 			echo "<ul class='subcats'>";
 			//loop through each subcatagory now
@@ -26,7 +26,7 @@ function show_catagories($results) {
 			while ($sub = mysqli_fetch_row($subresults)){
 				$sub_link = $sub[0] . '.php';
 				echo "<li>";
-				echo "<href='$sub_link'>"; //subcatagory name
+				echo "<a href='$sub_link'>"; //subcatagory name
 				echo "</li>";
 			}
 			echo "</ul>"; 
