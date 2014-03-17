3043910f1a51e034ecaa445414b32bbe035ea080
Alex Lambson <underoathalex15@gmail.com>
2014-03-17 07:04:45 -0600

    more css to headings

--- class/db_functions.php
+++ class/db_functions.php
@@ -17,7 +17,7 @@ function show_catagories($results) {
 		echo "<div class='column'>";
 			echo "<div class='listheads'>";
 			$link_name = $row[0] . '.php';
-				echo "<a href='$link_name'>$row[0]</a>"; //catagory name
+				echo "<a href='$link_name' class='listings'>$row[0]</a>"; //catagory name
 			echo "</div>"; //close listhead
 			echo "<ul class='subcats'>";
 			//loop through each subcatagory now
