634021568ff97a8c8014359bbc71d4cd994e91b0
Alex Lambson <underoathalex15@gmail.com>
2014-03-17 17:40:12 -0600

    404

--- class/404.php
+++ class/404.php
@@ -1,6 +1,8 @@
 <?php include_once('head.php');?>
 <div id="middlecolumn">
-	Looks like you Got lost! Here's some music to cheer you up!
-    <video id="lost" src="https://www.youtube.com/watch?v=32eywT-bQhQ" controls>You need HTML 5</video>
+	Looks like you Got lost! Here's some music to cheer you up!<br>
+    <div id="lost">
+    	<iframe width="560" height="315" src="//www.youtube.com/embed/32eywT-bQhQ" frameborder="0" allowfullscreen></iframe>
+    </div>
 </div>
 <?php include_once('foot.php');?>
