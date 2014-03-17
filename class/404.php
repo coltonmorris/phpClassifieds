93ca413071422688cf4d70182b63b01cf3e0618a
Alex Lambson <underoathalex15@gmail.com>
2014-03-17 17:37:00 -0600

    404 page

---
+++ class/404.php
@@ -0,0 +1,6 @@
+<?php include_once('head.php');?>
+<div id="middlecolumn">
+	Looks like you Got lost! Here's some music to cheer you up!
+    <video id="lost" src="https://www.youtube.com/watch?v=32eywT-bQhQ" controls>You need HTML 5</video>
+</div>
+<?php include_once('foot.php');?>
