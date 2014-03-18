27b8f3058ef22bb3445b3f8b788890c8953e4a3a
Alex Lambson <underoathalex15@gmail.com>
2014-03-17 18:02:09 -0600

    404

--- class/404.php
+++ class/404.php
@@ -1,5 +1,8 @@
 <?php include_once('head.php');?>
 <div id="middlecolumn">
+	<h2>
+    	404!
+    </h2>
 	Looks like you Got lost! Here's some music to cheer you up!<br>
     <div id="lost">
     	<iframe width="560" height="315" src="//www.youtube.com/embed/32eywT-bQhQ" frameborder="0" allowfullscreen></iframe>
