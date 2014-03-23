8d9d0bcd6a758a31c85eda7d2abd1423f7b598f0
coltonmorris <coltonmorris94@gmail.com>
2014-03-23 12:44:05 -0600

    individual page generation

--- class/individuallisting.php
+++ class/individuallisting.php
@@ -5,6 +5,7 @@ $id = $_GET['id'];
 $query = "select * from listings where id='$id'";
 $results = do_query($query);
 show_table($results);
+$query = "select * from listings where id='$id'";
 while ($row = mysqli_fetch_assoc($results)){
 	foreach ($row as $k =>$val){
 		echo $k. ' '.$val;
