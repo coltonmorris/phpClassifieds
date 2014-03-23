a7dc6bb6ced27c996c1d35d356acc933d1a63798
coltonmorris <coltonmorris94@gmail.com>
2014-03-23 13:13:44 -0600

    individual page generation

--- class/individuallisting.php
+++ class/individuallisting.php
@@ -12,7 +12,8 @@ while ($row = mysqli_fetch_assoc($results)){
 	$imageTableName = "image_$count";
 	foreach ($row as $k =>$val){
 		echo $k. ' ';
-//		if ($k == $imageTableName){
+		if ($k == $imageTableName){
+			echo "yay	A;LSDKFJA;LSKDFJAL;KDSFJ";
 //			//echo "<img src = $val>";
 //		}
 	}
