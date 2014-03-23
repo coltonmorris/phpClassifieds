cd3306bbb1bd8a2c9fc0d1113674bc5a8942698c
coltonmorris <coltonmorris94@gmail.com>
2014-03-23 12:56:52 -0600

    individual page generation

--- class/individuallisting.php
+++ class/individuallisting.php
@@ -7,10 +7,15 @@ $results = do_query($query);
 show_table($results);
 $query = "select * from listings where id='$id'";
 $results = do_query($query);
+$count = 0;
 while ($row = mysqli_fetch_assoc($results)){
 	foreach ($row as $k =>$val){
-		echo $k. ' '.$val;
+		$imageTableName = "image_$count";
+		if ($k == $imageTableName){
+			echo "<img src = $val>";
+		}
 	}
+	count ++;
 }
 echo "hello world";
 ?>
