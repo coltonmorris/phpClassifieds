453731f0b82d7ad96be2de6429d4f44568b4ebf7
Alex Lambson <underoathalex15@gmail.com>
2014-03-27 16:37:49 -0600

    mmm cv

--- class/listings.php
+++ class/listings.php
@@ -18,6 +18,58 @@ show_table($results);
         	Picture <!--Only diplays if listing has a picture-->
         </div>
     </div>
+    <div class="lItem">
+    	<div class="lDate">
+        	<p>
+        		November 28
+            </p>
+        </div>
+        <div class="lSubject">
+        	<a href="Link to the listing"><!--this will be listing.subject text-->This is my pretty poodle</a>
+        </div>
+        <div class="lPicture">
+        	Picture <!--Only diplays if listing has a picture-->
+        </div>
+    </div>
+    <div class="lItem">
+    	<div class="lDate">
+        	<p>
+        		November 28
+            </p>
+        </div>
+        <div class="lSubject">
+        	<a href="Link to the listing"><!--this will be listing.subject text-->This is my pretty poodle</a>
+        </div>
+        <div class="lPicture">
+        	Picture <!--Only diplays if listing has a picture-->
+        </div>
+    </div>
+    <div class="lItem">
+    	<div class="lDate">
+        	<p>
+        		November 28
+            </p>
+        </div>
+        <div class="lSubject">
+        	<a href="Link to the listing"><!--this will be listing.subject text-->This is my pretty poodle</a>
+        </div>
+        <div class="lPicture">
+        	Picture <!--Only diplays if listing has a picture-->
+        </div>
+    </div>
+    <div class="lItem">
+    	<div class="lDate">
+        	<p>
+        		November 28
+            </p>
+        </div>
+        <div class="lSubject">
+        	<a href="Link to the listing"><!--this will be listing.subject text-->This is my pretty poodle</a>
+        </div>
+        <div class="lPicture">
+        	Picture <!--Only diplays if listing has a picture-->
+        </div>
+    </div>
 </div>
 
 <?php include_once('foot.php'); ?>
