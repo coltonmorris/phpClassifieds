dca34b02ddfad085238da0f19e99ab8129551d72
Alex Lambson <underoathalex15@gmail.com>
2014-03-27 16:16:52 -0600

    listing page will be broken

--- class/listings.php
+++ class/listings.php
@@ -1,10 +1,21 @@
 <?php include_once('head.php');?>
 <div id="listings">
-<?php 
+<!--?php 
 $query = "select * from listings";
 $results = do_query($query);
 show_table($results);
-?>
+?-->
+	<div class="lItem">
+    	<div class="lDate">
+        	March 28
+        </div>
+        <div class="lSubject">
+        	<a href="Link to the listing"><!--this will be listing.subject text-->This is my pretty poodle</a>
+        </div>
+        <div class="lPicture">
+        	Picture <!--Only diplays if listing has a picture-->
+        </div>
+    </div>
 </div>
 
 <?php include_once('foot.php'); ?>
