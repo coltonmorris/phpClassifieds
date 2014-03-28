3e465e13c918951d17e41ff7b916960a0560501b
Colton <coltonmorris94@gmail.com>
2014-03-28 11:18:23 -0600

    individual listing modulation

--- class/db_functions.php
+++ class/db_functions.php
@@ -18,11 +18,11 @@ function search_function(){
 	echo "<form action='<?=$_SERVER[PHP_SELF]?>' method='GET'>";
 	echo "	<label for='subject'>Keyword:</label> ";
 	echo "  <input type='text' name='keyword' maxlength='68'><br><br>";
-	echo "	<input type='text' name='catagory' value=<?=$catagory?>><br><br>";
+	echo "	<input type='text' name='catagory' value='$catagory'><br><br>";
 	if (isset($_GET['subcatagory'])){
 		$subcatagory = $_GET['subcatagory'];
 	echo "	<input type='text'
-					name='subcatagory' value='<?=$subcatagory?>'><br><br>";
+					name='subcatagory' value='$subcatagory'><br><br>";
 	}
 //	echo "        <script type='text/javascript'>";
 //	echo "			$(document).ready(function() {";
