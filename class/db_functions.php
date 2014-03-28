be6e23ba5a44b157ef0fae9fc526461794714603
coltonmorris <coltonmorris94@gmail.com>
2014-03-27 18:34:08 -0600

    fixing image count uploading

--- class/db_functions.php
+++ class/db_functions.php
@@ -63,7 +63,7 @@ function show_listings($results) {
 				}
 				else if ($k == 'image_count'){
 					//camera only displays if listings has a picture
-					if ($k > 0){
+					if ($val > 0){
 					echo "<div class='lPicture'>";
 					echo "<img src='camera.png'>";
 					echo "</div>";
