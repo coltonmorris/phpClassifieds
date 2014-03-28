95594202bed869e56c39b813627142c1b4ff5a7a
Alex Lambson <underoathalex15@gmail.com>
2014-03-27 22:41:05 -0600

    changes

--- class/create_user.php
+++ class/create_user.php
@@ -1,28 +1,35 @@
 <?php include_once('head.php')?>
-<div id="middlecolumn">
-<p>
+<div class="content">
 	CREATE ACCOUNT:
 <form method="POST" action="<?$_SERVER['PHP_SELF']?>">
-		Username:<input type="text" name="username"><br/>
-		Password:<input type="text" name="password"><br/>
-		Email:<input type="text" name="email"><br/>
+		Username: <input type="text" required name="username"><br/>
+		Password: <input type="password" required name="password"><br/>
+        Confirm Password: <input type="password" required name="confirm"><br />
+		Email: <input type="text" required name="email"> *use your dmail<br/>
 		<input type="submit" value="Create Account" name="new_user">
 </form> 
-</p>
 <?php
 if (isset($_POST['new_user'])){
 		logout();
 		$username = $_POST['username'];
 		$password = md5($_POST['password'].$_POST['username']);
+		$passconfirm = md5($_POST['password'].$_POST['username']);
 		$email = $_POST['email'];
 		$allowed_domain = array('dmail.dixie.edu');
 		$RoleID = 0;
+		if ($password != $passconfirm){
+			echo "Passwords do not match!";
+			break;
+		}
 		if (filter_var($email, FILTER_VALIDATE_EMAIL))
 		{
 			$domain = array_pop(explode('@', $email));
 			if ( ! in_array($domain, $allowed_domain))
 			{
 				echo "you must use your dmail!";
+			}
+			else if ($password != $passconfirm){
+				echo "Passwords do not match!";
 			}
 			else
 			{
@@ -31,6 +38,5 @@ if (isset($_POST['new_user'])){
 		}
 }
 ?>
-</p>
 </div>
 <?php include_once('foot.php');?>
