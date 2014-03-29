ae44f0f494740317b99af0561b489096a7c965f1
coltonmorris <coltonmorris94@gmail.com>
2014-03-28 20:57:01 -0600

    individual listing modulation

--- class/postedinfo.php
+++ class/postedinfo.php
@@ -24,7 +24,9 @@ $id = $row[0];
 //PROBLEM: FILE EXTENSIONS ARE NOT REMEMBERED
 //for example: images/colton0201 through images/colton0204
 //if image count was 4.
+echo "<PRE>";
 print_r($_FILES);
+echo "</PRE>";
 $image_count = count($_FILES['images']['name']);
 echo $image_count;
 //planning on the base image being their listing id + username + index
