87a99475f16bdec2ecd8ba9781ead813266bfec8
Alex Lambson <underoathalex15@gmail.com>
2014-03-28 20:27:34 -0600

    oooo kill em

--- class/post.php
+++ class/post.php
@@ -1,25 +1,32 @@
 <?php include_once('head.php') ?>
-<div id="middlecolumn">
+<div class="content">
+		<script>
+			function limit(element, maxchars) {
+				el = element.value; //set the value to manipulate
+				flip = false; //check to see if we had to flip int to string
+				if (typeof el == "number") //see if el is a number
+					{
+						el = el.tostring().substr(0,maxchars);//convert and splice el	
+						flip = true;//set flip to skip string handlers
+					}
+				if (element.value.length > maxchars) //see if the incoming value is greater than maxcharacters
+					if (!flip){ //if it was a number, skip this section
+						el = el.substr(0,maxchars); //oh kill em
+						element.value = el; //return el
+					}
+					else {
+						element.value = Number(el); //return el as a number
+					}
+			}
+		</script>
 <form action="postedinfo.php" method="POST" enctype="multipart/form-data">
 	<div>
 		<label for="subject">*Subject:</label> 
-        <input type="text" required name="subject" maxlength="68"><br><br>
-        <script>
-			subject.oninput = function () {
-				if (this.value.length > 68)
-					this.value = this.value.slice(0,67); 
-			}
-		</script>
+        <input type="text" required name="subject" maxlength="68" onKeyDown="limit(this, 68);"><br><br>
 		<label for="description">*Description: </label>
         <textarea name="description" required cols="60" rows="5"></textarea><br><br>
 		<label for="cost">*Cost: </label>
-        <input type="number" max="99999" required name="cost"><br><br>
-        <script>
-			cost.oninput = function () {
-				if (this.value.length > 5)
-					this.value = this.value.slice(0,4); 
-			}
-		</script>
+        <input type="number" min="0" max="99999" required name="cost" onKeyDown="limit(this, 5);"><br><br>
         <script type="text/javascript">
 			$(document).ready(function() {
 				//alert('Document is ready');
