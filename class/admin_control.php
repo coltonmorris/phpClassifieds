29f9be7d617c0c2711665a7fbad58ef646a005cd
coltonmorris <coltonmorris94@gmail.com>
2014-03-31 00:16:04 -0600

    Fixing admin control

--- class/admin_control.php
+++ class/admin_control.php
@@ -1,20 +1,21 @@
 <?php 
 include_once('head.php');
+//do not allow people access to page is they are not an admin
 if ($_SESSION['RoleID'] != 1){
-	echo 'not allowed';
-	header('Location: index.php');
+	echo "<div class='listheads'>Listings</div>";
+	$username = $_SESSION['username'];
+	$password = $_SESSION['password'];
+	$query = "select * from listings";
+	$results = do_query($query);
+	show_listings_admin($results);
+	echo "<hr>";
+	echo "<div class='listheads'>Users</div>";
+	$query = "select * from users";
+	$results = do_query($query);
+	show_user_admin($results);
 }
-//if (!isset($_SESSION['control_panel']) || $_SESSION['control_panel'] != 'admin_control.php' && $_SESSION['RoleID'] != 1){
-echo "<div class='listheads'>Listings</div>";
-$username = $_SESSION['username'];
-$password = $_SESSION['password'];
-$query = "select * from listings";
-$results = do_query($query);
-show_listings_admin($results);
-echo "<hr>";
-echo "<div class='listheads'>Users</div>";
-$query = "select * from users";
-$results = do_query($query);
-show_user_admin($results);
+else {
+	echo "You do not have permission to visit this page";
+}
 ?>
 <?php include_once('foot.php') ?>
